33be11b74b74f0b8dc1c6843ed8a072bc65a7070
Shucai Xiao <Shucai.Xiao@amd.com>
2021-05-21 10:00:29 -0500

    add the python version of the migraphx_test_runner version

---
+++ migraphx_test_runner/set_path.sh
@@ -0,0 +1,4 @@
+#export LD_LIBRARY_PATH=/home/scxiao/Workplace/projects/AMDMIGraphX/deps_py/lib:/home/scxiao/Workplace/software/migraphlibs:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/scxiao/Workplace/projects/AMDMIGraphX/build/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/scxiao/Workplace/projects/AMDMIGraphX/build/lib:$PYTHONPATH
+
