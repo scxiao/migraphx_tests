ceed223ba615aef32d120b8ebf6bffc55f12817b
Shucai Xiao <Shucai.Xiao@amd.com>
2021-06-17 23:14:34 -0500

    add a tool to read protobuf data file and print out data

---
+++ onnx_tools/read_pb.py
@@ -0,0 +1,42 @@
+import sys, os
+import numpy as np
+import argparse
+import onnx
+from onnx import numpy_helper
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Read protobuf file")
+    parser.add_argument('file_name',
+                        type=str,
+                        metavar='file_name',
+                        help='protobuf data file name')
+    args = parser.parse_args()
+
+    return args
+
+
+def read_pb_file(filename):
+    try:
+        pfile = open(filename, 'rb')
+    except IOError:
+        print("File {} open error".format(filename))
+        sys.exit(1)
+
+    data_str = pfile.read()
+    tensor = onnx.TensorProto()
+    tensor.ParseFromString(data_str)
+    np_array = numpy_helper.to_array(tensor)
+    print("input_shape = {}".format(np_array.shape))
+
+    return np_array
+
+
+def main():
+    args = parse_args()
+    file_name = args.file_name
+
+    read_pb_file(file_name)
+
+
+if __name__ == "__main__":
+    main()
