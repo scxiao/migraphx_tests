e638c0ea07841b904b024b1ad9918f85a7189e97
Shucai Xiao <Shucai.Xiao@amd.com>
2022-01-26 09:36:43 -0600

    fix c++ code build error

--- onnxruntime/test_char_rnn.cpp
+++ onnxruntime/test_char_rnn.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <map>
 #include <cstdlib>
+#include <numeric>
+#include <algorithm>
+#include <climits>
 
 // onnx runtime include
 #include <core/session/onnxruntime_cxx_api.h>
-#include <core/framework/allocator.h>
+// #include <core/framework/allocator.h>
 #include <core/providers/migraphx/migraphx_provider_factory.h>
 
 std::string get_type_name(ONNXTensorElementDataType type)
@@ -118,23 +121,23 @@ int main(int argc, char **argv)
         std::cout << ", type: " << get_type_name(onnx_type) << ", shape = ";
         print_vec(out_dims[i]);
 
-        std::size_t elem_num = std::accumulate(in_dims[i].begin(), in_dims[i].end(), 1, std::multiplies<int64_t>());
+        std::size_t elem_num = std::accumulate(out_dims[i].begin(), out_dims[i].end(), 1, std::multiplies<int64_t>());
         output_data[i].resize(elem_num);
         outputs.push_back(Ort::Value::CreateTensor<float>(memory_info, output_data[i].data(), output_data[i].size(),
                     out_dims[i].data(), out_dims[i].size()));
     }
     std::cout << std::endl;
 
-std::cout << "Before execution...." << std::endl;
+    std::cout << "Start inference ...." << std::endl;
     sess.Run(Ort::RunOptions{nullptr}, input_names, inputs.data(), inputs.size(), output_names,
             outputs.data(), outputs.size());
-std::cout << "After execution...." << std::endl;
+    std::cout << "Finished inference ...." << std::endl;
 
-    std::cout << "outputs = " << std::endl;
+    std::cout << "outputs: " << std::endl;
     std::size_t i = 0;
     for (auto& output : output_data)
     {
-        std::cout << "Output " << i++ << " = " << std::endl;
+        std::cout << "Output_" << i++ << " = " << std::endl;
         print_vec(output);
         std::cout << std::endl;
     }
